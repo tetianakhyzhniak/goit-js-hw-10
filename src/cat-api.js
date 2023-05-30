4f1e9d154d451732c95ee76fc0c35eba9c851fb3
Tetiana <tanya200291@ukr.net>
2023-05-30 21:31:52 +0200

    hw-10/1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const api_key= 'live_wgNTAeN88icjGIlOhkcRfT4NUokIxScBWIUBGF3hxxFEeCT8oTfofBfbuUqfmBxP';
-const errorParagraph = document.querySelector('p.error');
-errorParagraph.style.display = 'none';
+
+
 export function fetchBreeds(){
  return fetch(`${BASE_URL}breeds?api_key=${api_key}`)
     .then(resp => {
@@ -9,17 +9,8 @@ export function fetchBreeds(){
             throw new Error (resp.statusText);
         }
         return resp.json()
-    }).then(data=> {
-        return data.map(breed => ({
-            id: breed.id,
-            name: breed.name,
-          }));
     })
 }
-
-fetchBreeds().then(cat => console.log(cat)).catch(error => {console.log(error);
-errorParagraph.textContent = error.message; 
-errorParagraph.style.display = 'block'; });
 
 export function fetchCatByBreed(breedId){
 return fetch(`${BASE_URL}images/search?api_key=${api_key}&breed_ids=${breedId}`)
@@ -28,18 +19,5 @@ return fetch(`${BASE_URL}images/search?api_key=${api_key}&breed_ids=${breedId}`)
         throw new Error (resp.statusText);
     }
     return resp.json();
-}).then(data => {
-    if (data && data.length>0){
-const catInfo = data[0].breeds[0];
-console.log(catInfo)
-return {
-    images: data[0].url,
-    breedName: catInfo.name,
-    description: catInfo.description,
-    temperament: catInfo.temperament,
-  };
-} 
-}).catch(error => {
-    throw new Error("Not found cat");
-  });
+})
 }
--- src/index.js
+++ src/index.js
@@ -1,47 +1,53 @@
 
-import SlimSelect from 'slim-select';
+// import SlimSelect from 'slim-select';
+  // new SlimSelect({
+    //     select: '.breed-select',
+    //   })
 import * as catApi from './cat-api';
-
-document.addEventListener('DOMContentLoaded', () =>{
-    new SlimSelect({
-        select: '#breed-select',
-        searchable: false,
-      })
-})
-
 
 const loader = document.querySelector('.loader');
 const catInfoContainer = document.querySelector('.cat-info');
 const selectBreed= document.querySelector('.breed-select');
+const errorParagraph = document.querySelector('p.error');
+errorParagraph.style.display = 'none'
 
-selectBreed.addEventListener('change', OnChange)
-const selectCatByBreed = selectBreed.value;
-function OnChange(){
-// catApi.fetchCatByBreed()
+catApi.fetchBreeds().then(data=> {
+selectBreed.style.display = 'none';
+loader.style.display = 'block';
+    return data.map(breed => ({
+        id: breed.id,
+        name: breed.name,
+      }));
+})
+.then(data => {
+selectBreed.style.display = 'block';
+loader.style.display = 'none';
+    const markUp = data.map(({id, name}) => `<option value="${id}">${name}</option>`).join('');
+    selectBreed.insertAdjacentHTML('beforeend', markUp);
+  
+})
+.catch((error) => {console.log(error);
+    errorParagraph.style.display = 'block'
+});
 
+selectBreed.addEventListener('change', onChange)
+function onChange(e){
+e.preventDefault();
+loader.style.display = 'block';
+errorParagraph.style.display = 'none'; 
+catApi.fetchCatByBreed(e.target.value).then(data => {
+    loader.style.display = 'none'
+const catInfoHtml = data.map(({breeds, url})=> `<img src="${url}" width = 300>
+<p>Breed: ${breeds[0].name}</p>
+<p>Description: ${breeds[0].description}</p>
+<p>Temperament: ${breeds[0].temperament}</p>`).join('');
+catInfoContainer.innerHTML = catInfoHtml;
+}).catch((error) => {console.log(error);
+    errorParagraph.style.display = 'block';
+})
 }
 
-function getCatInfo(selectCatByBreed){
-    return catApi.fetchCatByBreed(selectCatByBreed)
-    .then((catInfo) => {
-        const catInfoHtml =` <img src="${catInfo.imageUrl}">
-        <p>Breed: ${catInfo.breedName}</p>
-        <p>Description: ${catInfo.description}</p>
-        <p>Temperament: ${catInfo.temperament}</p>`;
-        catInfoContainer.innerHTML = catInfoHtml;
-    }).catch(error => console.log(error));
-}
-catApi.fetchCatByBreed()
-getCatInfo()
-//отримання кошар за породами
-catApi.fetchBreeds()
-.then((breeds) => breeds.forEach((breed) => {
-    let option = document.createElement('option');
-    option.value = breed.id;
-    option.textContent = breed.name;
-    selectBreed.appendChild(option);
-}));
-   
+
 
 
 
